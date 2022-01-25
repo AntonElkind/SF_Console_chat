7d57fab0bcdf064bad7543d08dee6e3046e23265
Anton Elkind <anton.elkind@gmail.com>
2022-01-25 22:14:15 +0200

    Add Container template class

---
+++ chat/chat/container.h
@@ -0,0 +1,126 @@
+#pragma once
+#include <exception>
+
+template <typename T>
+class Container
+{
+private:
+	int _length;
+	T* _data;
+
+public:
+	Container();
+	Container(const Container& other);
+	~Container();
+	Container& operator=(const Container& other);
+	T& operator[](int index);
+
+	int getLength();
+	void add(T element);
+};
+
+class BadRange : public std::exception
+{
+public:
+	virtual const char* what() const noexcept override
+	{
+		return "ERROR: Invalid array index!";
+	}
+};
+
+class BadSize : public std::exception
+{
+public:
+	virtual const char* what() const noexcept override
+	{
+		return "ERROR: Not enough space!";
+	}
+};
+
+/**
+* Template class implementation
+*/
+template <typename T>
+Container<T>::Container() : _length(0), _data(nullptr)
+{
+}
+
+template <typename T>
+Container<T>::Container(const Container& other) : Container()
+{
+	if (other._length)
+	{
+		_length = other._length;
+		_data = new T[this->_length];
+		for (int i = 0; i < this->_length; ++i)
+		{
+			_data[i] = other._data[i];
+		}
+	}
+}
+
+template <typename T>
+Container<T>::~Container()
+{
+	delete[] _data;
+}
+
+template <typename T>
+Container<T>& Container<T>::operator=(const Container& other)
+{
+	if (_length == 0)
+	{
+		if (other._length)
+		{
+			_length = other._length;
+			_data = new T[_length];
+			for (int i = 0; i < _length; ++i)
+			{
+				_data[i] = other._data[i];
+			}
+		}
+	}
+	else
+	{
+		if (_length < other._length)
+		{
+			throw BadSize();
+		}
+		for (int i = 0; i < other._length; ++i)
+		{
+			_data[i] = other._data[i];
+		}
+	}
+	return *this;
+}
+
+template<typename T>
+T& Container<T>::operator[](int index)
+{
+	if (index < 0 || index > _length - 1)
+	{
+		throw BadRange();
+	}
+	return _data[index];
+}
+
+template <typename T>
+int Container<T>::getLength()
+{
+	return _length;
+}
+
+template <typename T>
+void  Container<T>::add(T element)
+{
+	T* dataTemp = new T[_length + 1];
+	int i;
+	for (i = 0; i < _length; ++i)
+	{
+		dataTemp[i] = _data[i];
+	}
+	dataTemp[i] = element;
+	delete[] _data;
+	_data = dataTemp;
+	++_length;
+}
